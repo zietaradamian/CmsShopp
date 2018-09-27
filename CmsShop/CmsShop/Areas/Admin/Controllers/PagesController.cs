52447f28ba6797128c96bf3c5791fbc442350296
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-09-27 16:27:34 +0200

    admin pages / dodanie listy stron (cz2)

--- CmsShop/CmsShop/Areas/Admin/Controllers/PagesController.cs
+++ CmsShop/CmsShop/Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Pages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace CmsShop.Areas.Admin.Controllers
 {
@@ -7,7 +11,21 @@ namespace CmsShop.Areas.Admin.Controllers
         // GET: Admin/Pages
         public ActionResult Index()
         {
-            return View();
+            //Deklaracja listy pagevm
+            List<PageVM> pagesList;
+
+            
+            using (Db db = new Db())
+            {
+                //Inicjalizacja listy
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x=> new PageVM(x)).ToList();
+
+
+            }
+
+
+            //zwracanie stron do widoku
+            return View(pagesList);
         }
     }
 }
