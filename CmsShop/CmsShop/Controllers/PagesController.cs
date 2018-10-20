e184a8f6117a65fdf7fcfa5c5f38aaecbbbbb8aa
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-10-20 17:30:19 +0200

    FrontPages / dodanie controlera i widoku i routingu cz.1

---
+++ CmsShop/CmsShop/Controllers/PagesController.cs
@@ -0,0 +1,58 @@
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShop.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Index/{pages}
+        public ActionResult Index(string page="")
+        {
+            //ustawiamy adres naszej strony
+            if (page == "")
+            {
+                page = "home";
+            }
+            //deklaracja pageVM i pageDTO
+            PageVM model;
+            PageDTO dto;
+            //Sprawdzamy czy strona istnieje
+            using (Db db = new Db())
+            {
+                if (!db.Pages.Any(x=> x.Slug.Equals(page)))
+                {
+                    return RedirectToAction("Index", new { page="" });
+                }
+            }
+
+            //pobieramy pageDto 
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+            //ustawianie tytułu naszej strony
+
+            ViewBag.PageTitle = dto.Title;
+
+            //sprawdzaamy ma pasek boczny (sidebar)
+
+            if (dto.HasSideBar == true)
+            {
+                ViewBag.Sidebar = "Tak";
+            }
+            else
+            {
+                ViewBag.Sidebar = "Nie";
+            }
+            //inizjalizacja pageVm
+            model = new PageVM(dto);
+            //zwracanie widoku z pagevm
+            return View(model);
+        }
+    }
+}
