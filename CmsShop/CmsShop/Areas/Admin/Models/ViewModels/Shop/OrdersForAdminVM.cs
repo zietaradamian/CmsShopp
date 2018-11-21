bd2ed5704e7253601ff35700ead515ee3dba18d0
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-11-21 15:56:50 +0100

    Zamówienia / cz5

---
+++ CmsShop/CmsShop/Areas/Admin/Models/ViewModels/Shop/OrdersForAdminVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CmsShop.Areas.Admin.Models.ViewModels.Shop
+{
+    public class OrdersForAdminVM
+    {
+        public int OrderNumber { get; set; }
+        public string UserName { get; set; }
+        public decimal Total { get; set; }
+        public Dictionary<string,int> ProductsAndQty { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
