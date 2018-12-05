f24b1bb2ef2dc67aac5212687c3d8896a7d55b89
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-12-05 17:45:01 +0100

    Przejscie na szablon AdminLte

--- CmsShop/CmsShop/App_Start/BundleConfig.cs
+++ CmsShop/CmsShop/App_Start/BundleConfig.cs
@@ -21,14 +21,31 @@ namespace CmsShop
             // Użyj wersji deweloperskiej biblioteki Modernizr do nauki i opracowywania rozwiązań. Następnie, kiedy wszystko będzie
             // gotowe do produkcji, użyj narzędzia do kompilowania ze strony https://modernizr.com, aby wybrać wyłącznie potrzebne testy.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                     "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/admin-lte/js").Include(
+                      "~/admin-lte/js/adminlte.js",
+                      "~/admin-lte/plugins/fastclick/fastclick.js",
+                      "~/admin-lte/plugins/datatables/jquery.dataTables.js",
+                      "~/admin-lte/plugins/datatables/dataTables.bootstrap.js",
+                      "~/admin-lte/plugins/chartjs/Chart.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/font-awesome.css",
+                      "~/Content/ionicons.css",
+                      "~/admin-lte/css/AdminLTE.css",
+                      "~/admin-lte/css/skins/skin-blue.css",
+                      "~/admin-lte/plugins/datatables/dataTables.bootstrap.css"));
         }
     }
 }
--- CmsShop/CmsShop/Models/Data/OrderDetailsDTO.cs
+++ CmsShop/CmsShop/Models/Data/OrderDetailsDTO.cs
@@ -11,6 +11,7 @@ namespace CmsShop.Models.Data
     public class OrderDetailsDTO
     {
         [Key]
+        public int Id { get; set; }
         public int OrderId { get; set; }
         public int UserId { get; set; }
         public int ProductId { get; set; }
